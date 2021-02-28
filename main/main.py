0b1c8942c26cf00efd4fdc3920a1a45d9b087eb9
Niall McCarra <niallmccarra@Nialls-MacBook-Pro.local>
2021-02-28 22:34:12 +0000

    master: add config parser

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+from configparser import ConfigParser
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Retrieve setlist from YouTube video")
@@ -6,9 +8,15 @@ if __name__ == '__main__':
                         default="https://www.youtube.com/watch?v=eFhsH3j4fOU&t=2170s",
                         help="YouTube video URL")
     parser.add_argument('-config_path', '--config_path',
-                        default="/main/resources/config.ini",
+                        default="./main/resources/config.ini",
                         help="path to .ini file with configuration for this application")
     args = parser.parse_args()
 
+    config = ConfigParser()
+    config.read(args.config_path)
+    acr_config = config['acr-cloud']
+
     print(args.url)
     print(args.config_path)
+    print(dict(acr_config))
+
