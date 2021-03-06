68925abf3c9377eaef10706d450fe5b562f5df85
Niall McCarra <niallmccarra@Nialls-MacBook-Pro.local>
2021-03-06 00:05:24 +0000

    add-acr-client: add marshalling

--- main/main.py
+++ main/main.py
@@ -1,8 +1,9 @@
 from configparser import ConfigParser
 from flask import Flask
 from flask import request
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 from services.acr_cloud_client import ACRCloudClient
+import json
 
 config = ConfigParser()
 config.read("./main/resources/config.ini")
@@ -11,7 +12,21 @@ app_config = config["app"]
 acr_cloud_request_config = config["acr-cloud-request"]
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version=app_config["version"], title="SETGRAB",
+          default='All', default_label='Recognise set lists')
+
+# request body models
+acr_cloud_config = api.model("acr_cloud_config", {
+    "host": fields.String(required=True, description=""),
+    "access_key": fields.String(required=True, description=""),
+    "access_secret": fields.String(required=True, description=""),
+    "timeout": fields.Integer(required=False, description="")
+})
+
+acr_cloud_song = api.model("acr_cloud_song", {
+    'acr_cloud_config': fields.Nested(acr_cloud_config),
+    'file_path': fields.String(required=True, description='')
+})
 
 
 @api.route("/home")
@@ -22,12 +37,15 @@ class Home(Resource):
 
 @api.route("/acr-cloud/recognise/song")
 class ACRCloudRecognizeSong(Resource):
+    @api.expect(acr_cloud_song, validate=True)
     def post(self):
         start_seconds = int(acr_cloud_request_config["start_seconds"])
         rec_length = int(acr_cloud_request_config["rec_length"])
         request_body = request.get_json()
         acr_cloud_client = ACRCloudClient(config=dict(request_body["acr_cloud_config"]))
-        return acr_cloud_client.recognize_by_file(request_body["file_path"], start_seconds, rec_length, None, 4)
+        return json.loads(
+            acr_cloud_client.recognize_by_file(request_body["file_path"], start_seconds, rec_length, None, 4)
+        )
 
 
 if __name__ == '__main__':
