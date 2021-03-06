a6a641ce0167c91fd9a5e6258ca42e0d45285cec
Niall McCarra <niallmccarra@Nialls-MacBook-Pro.local>
2021-03-06 17:22:37 +0000

    master: add docstrings for ACRCloudClient

--- main/services/acr_cloud_client.py
+++ main/services/acr_cloud_client.py
@@ -5,7 +5,15 @@ import json
 
 
 class ACRCloudClient(ACRCloudRecognizer):
+    """
+    A class which extends the ACRCloudRecognizer by allowing for multiple file
+    recognition
+    """
     def __init__(self, config, acr_cloud_config):
+        """
+        :param config: full config file in main/resources/config.ini
+        :param acr_cloud_config: configuration for the ACR Cloud API i.e. credentials
+        """
         self.config = config
         self.acr_cloud_request_config = self.config["acr-cloud-request"]
         self.start_seconds = int(self.acr_cloud_request_config["start_seconds"])
@@ -14,8 +22,18 @@ class ACRCloudClient(ACRCloudRecognizer):
         super().__init__(config=acr_cloud_config)
 
     def recognize_song(self, file_path):
+        """
+        recognize a single song by file
+        :param file_path: path to audio file
+        :return: a dict of the song attributes
+        """
         return json.loads(self.recognize_by_file(file_path, self.start_seconds, self.rec_length, None))
 
     def recognize_multiple(self, folder_path):
+        """
+        recognize the songs in a given folder
+        :param folder_path: folder containing audio files
+        :return: dict object of filename to song attributes
+        """
         onlyfiles = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
         return {f.rsplit('.', 1)[0]: self.recognize_song(folder_path + "/" + f) for f in onlyfiles}
